f9bec6ace40fc2cacfc611bc796d440fa56164a5
[Abdullah] <[Abdullah@gmail.com]>
2026-09-17 15:56:32 +0300

    edit splash

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,10 +1,28 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/core/routes/route_name.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
+
+  void _navigateNext(BuildContext context) {
+    bool isLoggedIn = false;
+    try {
+      isLoggedIn = FirebaseAuth.instance.currentUser != null;
+    } catch (_) {
+      isLoggedIn = false;
+    }
+
+    if (context.mounted) {
+      if (isLoggedIn) {
+        Navigator.pushReplacementNamed(context, RouteName.layout);
+      } else {
+        Navigator.pushReplacementNamed(context, RouteName.onBoarding);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +34,9 @@ class SplashScreen extends StatelessWidget {
             Expanded(
               child: Center(
                 child: FadeInDown(
-                  onFinish: (_) {
-                    Navigator.pushReplacementNamed(
-                      context,
-                      RouteName.onBoarding,
-                    );
-                  },
+                  onFinish: (_) => _navigateNext(context),
                   animate: true,
-                  delay: Duration(seconds: 2),
+                  delay: const Duration(seconds: 2),
                   child: Image.asset(
                     'assets/logos/logo_app.png',
                     width: 121.w,
